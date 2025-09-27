3691a2027e5ba4e20c1be96416629335201cebc9
RadhaXRani <radha11189198141@gmail.com>
2025-09-27 16:25:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ====================
+# CONFIGURATION
+# ====================
+API_ID = int(os.environ.get("API_ID", "27546440"))
+API_HASH = os.environ.get("API_HASH", "3892f78baf81709ac1672ef1c24a3556")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7965557926:AAGhbSSvL4P12lE1_jbPNDBPr5XLLFkD5OE")
+OWNER_ID = int(os.environ.get("OWNER_ID", "7744878270"))
+PORT = int(os.environ.get("PORT", 8080))
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://RADHAXRANI:RADHAXRANI@cluster0.ftpb4.mongodb.net/?retryWrites=true&w=majority")
